54cbaf38fb4d10f7cf5251bf81f73e38dfa1c500
s-hackman <shisho.hackman@gmail.com>
2023-02-06 13:19:22 +0000

    add individual Article Page and a navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import "./App.css";
+import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ArticleList from "./components/articles/ArticleList";
+import "./App.css";
+import ArticlePage from "./components/articles/ArticlePage";
 import Error from "./components/Error";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Navbar />
         <Routes>
           <Route path="/" element={<ArticleList />} />
           <Route path="/articles" element={<ArticleList />} />
+          <Route path="/articles/:article_id" element={<ArticlePage />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </div>
---
+++ src/components/articles/ArticlePage.jsx
@@ -0,0 +1,94 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getArticleById } from "../../utils/api.js";
+import Error from "../Error";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+  Button,
+  Grid,
+  Box,
+} from "@mui/material";
+import TopicIcon from "@mui/icons-material/Topic";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import ThumbsUpDownIcon from "@mui/icons-material/ThumbsUpDown";
+import { motion } from "framer-motion";
+
+const ArticlePage = () => {
+  const [articleInfo, setArticleInfo] = useState([]);
+  const [err, setErr] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { article_id } = useParams();
+
+  useEffect(() => {
+    setIsLoading(true);
+    getArticleById(article_id)
+      .then((article) => {
+        setArticleInfo(article);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setErr(err.message);
+      });
+  }, [article_id]);
+
+  return (
+    <>
+      {err && <Error />}
+      {isLoading && <p>Loading</p>}
+      <section className="article-container">
+        <Card sx={{ mx: 1 }} className="card" variant="outlined">
+          <CardContent key={articleInfo.article_id}>
+            <Typography variant="h4">{articleInfo.title}</Typography>
+            <Typography color="textSecondary">
+              {`Posted by ${articleInfo.author} at ${new Date(
+                articleInfo.created_at
+              ).toGMTString()}`}
+            </Typography>
+            <hr />
+            <Box component="img" src={articleInfo.article_img_url} />
+            <br />
+            <Typography variant="p">{articleInfo.body}</Typography>
+            <hr />
+            <Grid container justifyContent="space-between">
+              <Typography color="textSecondary">
+                <ThumbsUpDownIcon align="left" sx={{ paddingRight: 1 }} />
+                {articleInfo.votes} Upvotes
+              </Typography>
+              <Typography color="textSecondary">
+                <TopicIcon />
+                {`${articleInfo.topic}`}
+              </Typography>
+            </Grid>
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="outlined"
+              size="small"
+              component={motion.button}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <ThumbUpIcon sx={{ paddingRight: 1 }} />
+              Like
+            </Button>
+            <Button
+              variant="outlined"
+              size="small"
+              component={motion.button}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <ThumbDownIcon sx={{ paddingRight: 1 }} /> Dislike
+            </Button>
+          </CardActions>
+        </Card>
+      </section>
+    </>
+  );
+};
+
+export default ArticlePage;
